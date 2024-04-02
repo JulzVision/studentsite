1b121ac79f06269e5ca91dff9c603bdff5b2a60d
JulzVision <juliusc@hawaii.edu>
2024-04-02 11:10:31 -1000

    logo size change, gradients, added footer

---
+++ art128/churrosamigos-v1/js/script.js
@@ -0,0 +1,13 @@
+const logo = document.querySelector(".logo-img")
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    logo.style.width = "10em";
+    logo.style.margin = "6em";
+  } else {
+    logo.style.width = "50em";
+    logo.style.margin = "0";
+
+  }
+}
