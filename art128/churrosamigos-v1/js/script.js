1e0e243ffecbc46f497924a7682f477dd1951317
JulzVision <juliusc@hawaii.edu>
2024-04-02 14:36:15 -1000

    js height auto

--- art128/churrosamigos-v1/js/script.js
+++ art128/churrosamigos-v1/js/script.js
@@ -5,6 +5,7 @@ function scrollFunction() {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
     logo.style.width = "10em";
     logo.style.margin = "6em";
+    logo.style.height = "auto";
   } else {
     logo.style.width = "50em";
     logo.style.margin = "0";
