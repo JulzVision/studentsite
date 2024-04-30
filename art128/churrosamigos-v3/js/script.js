4c553f399136bb7ee55ba2c4224a2bf4523b6321
JulzVision <juliusc@hawaii.edu>
2024-04-30 12:02:03 -1000

    first pass at parallax v3

--- art128/churrosamigos-v3/js/script.js
+++ art128/churrosamigos-v3/js/script.js
@@ -8,3 +8,15 @@ window.addEventListener("scroll", function(){
   parallax.style.backgroundPositionY = offset * 0.7 + 'px';
   
 })
+
+//similar and more recent setup I found. I think I'd just have to replace .images with .reef.
+
+const p_items = document.querySelectorAll('.parallax-wrapper .parallax-item');
+
+window.addEventListener('scroll', () => {
+
+  p_items[0].style.top = "-" + (window.scrollY / 2.5) + "px";
+  p_items[1].style.top = "-" + (window.scrollY / 3.5) + "px";
+  p_items[2].style.top = "-" + (window.scrollY / 4 ) + "px";
+
+});
